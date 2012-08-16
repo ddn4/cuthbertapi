8df21e3f4d023f124b08ccddf3739215de4c0e35
Joe Szod <joe.szod@gmail.com>
2012-08-16 13:16:39 -0400

    Forgot to add juxtapose.rb to tracked files in previous commit

---
+++ app/models/juxtapose.rb
@@ -0,0 +1,17 @@
+# Class used to interface with the juxtapose API
+class Juxtapose
+  # include Mongoid::Document
+  include HTTParty
+  format  :json
+  base_uri Configuration.juxtapose_api_url
+
+  def self.post_new_position(id, lat, lon, timestamp)
+    post('/users/' + id.to_s  + '/positions.json',
+      :query => { :position => {:latitude => lat, :longitude => lon, :timestamp => timestamp } }
+    )
+  end  
+
+  def self.get_users
+    get('/users.json')
+  end
+end
