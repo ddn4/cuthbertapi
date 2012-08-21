8c9481e91574176f6c64e754dd094975cef50f35
Joe Szod <joe.szod@gmail.com>
2012-08-21 15:04:02 -0400

    Fixing heroku build error - Added user path string concantenation to UserResource class

--- app/models/user_resource.rb
+++ app/models/user_resource.rb
@@ -1,6 +1,6 @@
 class UserResource < ActiveResource::Base
   # include Mongoid::Document
-  self.site = Configuration.juxtapose_api_url
+  self.site = Configuration.juxtapose_api_url << "user/:user_id"
   self.element_name = "user"
   self.format = :json
 
