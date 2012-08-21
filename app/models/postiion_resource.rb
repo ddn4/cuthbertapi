492fe1b12a3eeb80d7dff61a9818e1a65c0c2a60
Joe Szod <joe.szod@gmail.com>
2012-08-21 09:06:23 -0400

    Removed misnamed and duplicate PostiionResource

--- app/models/postiion_resource.rb
+++
@@ -1,6 +0,0 @@
-class PostiionResource < ActiveResource::Base
-  # include Mongoid::Document
-  self.site = Configuration.juxtapose_api_url + '/users/:user_id'
-  self.element_name = "position"
-  self.format = :json
-end
