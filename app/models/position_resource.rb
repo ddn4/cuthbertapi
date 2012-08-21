13eeaa1c4a0f0c7e9fe5454eed38a577a387022a
Joe Szod <joe.szod@gmail.com>
2012-08-21 14:53:59 -0400

    Fixing heroku build crash, moved string concantenation to a seperate line in PositionResource

--- app/models/position_resource.rb
+++ app/models/position_resource.rb
@@ -1,7 +1,8 @@
 class PositionResource < ActiveResource::Base
   # Position is a nested reource under User in Juxtapose
   # Append User to base site to hande nested route
-  self.site = Configuration.juxtapose_api_url << "users/:user_id"
+  self.site = Configuration.juxtapose_api_url ## << "users/:user_id"
+  self.site << "users/:user_id"
 
   self.element_name = "position"
   self.format = :json
